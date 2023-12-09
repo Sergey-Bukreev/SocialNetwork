bab78b0d32160ebc6b01e93b429d58387f1ac3d5
BukreevSergey <bukreevwork2@gmail.com>
2023-12-09 13:53:08 +0200

    use map for create JSX on dialogs block

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,43 +1,56 @@
 import React from 'react';
 import classes from "./Dialogs.module.css"
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-const DialogItem = (props:any) => {
-    let path = "/dialogs" + props.id;
+const DialogItem = (props: any) => {
+    let path = "/dialogs/" + props.id;
     return (
         <div className={classes.dialog}>
             <NavLink to={path}>{props.name}</NavLink>
         </div>
+    );
+}
+
+const Message = (props: any) => {
+    return (
+            <div  className={classes.message}>
+                {props.message}
+            </div>
     )
 }
 
-const Message = (props:any) => {
-    return (<div className={classes.message}>
-            {props.message}
-    </div>
-    )
-}
-export const Dialogs:React.FC = () => {
+export const Dialogs: React.FC = (props: any) => {
+
+    let DialogsData:{id:number, name:string}[] = [
+        {id: 1, name: "Dimych"},
+        {id: 2, name: "Sveta"},
+        {id: 3, name: "Sergio"},
+        {id: 4, name: "Alisa"},
+        {id: 5, name: "Victor"},
+        {id: 6, name: "Valera"}
+    ]
+
+    let MessageData:{id:number, message:string}[] = [
+        {id: 1, message: "Nice to meet you"},
+        {id: 2, message: "Hello"},
+        {id: 3, message: "How are you"},
+        {id: 4, message: "IT-Incubator"},
+    ]
+
+    let dialogsElements:JSX.Element[] = DialogsData.map((el:{id:number, name:string})=>
+    <DialogItem name={el.name} id={el.id} /> )
+
+    let messagesElements:JSX.Element[] = MessageData.map((el:{id:number, message:string}) =>
+     <Message message={el.message}/>)
+
     return (
         <div className={classes.dialogs}>
-                <div className={classes.dialogsItems}>
-                    <DialogItem name="Dimych" id="1"/>
-                    <DialogItem name="Sveta" id="2"/>
-                    <DialogItem name="Sergey" id="3"/>
-                    <DialogItem name="Alisa" id="4"/>
-                    <DialogItem name="Victor" id="5"/>
-                </div>
-
-            <div className={classes.messages}>
-                <Message message={"Hello"}/>
-                <Message message={"welcome to IT Incubator"}/>
-                <Message message={"Nice to meet you"}/>
-
+            <div className={classes.dialog}>
+                {dialogsElements}
             </div>
-            <div>
-
+            <div className={classes.messages}>
+                {messagesElements}
             </div>
         </div>
     );
 };
-
